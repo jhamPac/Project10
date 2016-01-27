22e55ce54ff2e10c6cf4181784d0995b70961cc3
Kevia <cloud@kevia.me>
2016-01-27 11:30:22 -0800

    layout set up done

---
+++ FilterMe/FilterMe/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  FilterMe
+//
+//  Created by jhampac on 1/27/16.
+//  Copyright © 2016 jhampac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
+    @IBAction func changeFilter(sender: AnyObject)
+    {
+        
+    }
+    
+    @IBAction func save(sender: UIButton)
+    {
+        
+    }
+    
+    @IBAction func intensityChanged(sender: UISlider)
+    {
+        
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
+
